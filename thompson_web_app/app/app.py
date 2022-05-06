6807d878b083fa54947310ac7636831c99b0fca4
elina <elinaisrayelyan@gmail.com>
2022-05-06 23:20:09 +0400

    finalize frontend

--- thompson_web_app/app/app.py
+++ thompson_web_app/app/app.py
@@ -1,148 +1,269 @@
-from dash import Dash, html, Input
+from dash import html, Dash, dcc, Input, Output, callback, State
 
-app = Dash(__name__)
+import dash_bootstrap_components as dbc
 
-app.layout = html.Div(style={'backgroundColor': '#85799F'}, children=[
-    html.Nav(children=[
-        html.Nav(children='Get Your Best Reward!',
-                 style={
-                     'textAlign': 'center',
-                     'color': 'white',
-                     'float': 'left',
-                     'font-size': '50px',
-                     'padding': '200px',
-                     'font-family': '-apple-system,system-ui,"Segoe UI",Roboto,"Helvetica Neue",Ubuntu,sans-serif',
-                     '-webkit-text-stroke': '3px black',
-                     'font-weight': 'bold'
+import base64
+import pandas as pd
+import io
+import plotly.express as px
+import pdfkit
+from time import sleep
 
-                 }
-                 ),
-        html.Nav(children='You Give Us Data We Find Your Reward',
-                 style={
-                     'textAlign': 'center',
-                     'color': 'white',
-                     'font-size': '50px',
-                     'background-image': 'url(https://images.pexels.com/photos/2786933/pexels-photo-2786933.jpeg?auto'
-                                         '=compress&cs=tinysrgb&w=1260&h=750&dpr=1)',
-                     'background-size': '100%',
-                     'padding': '200px',
-                     'font-family': '-apple-system,system-ui,"Segoe UI",Roboto,"Helvetica Neue",Ubuntu,sans-serif'
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-                 }
-                 )]),
-
-    html.Div(
-        children='''FindYourReward is a tool which enables users to input their sales data, define KPIs and recieve 
-        analysis on best reward-giving advertisement. '''
-        , style={
-            'textAlign': 'left',
-            'color': 'black',
-            'border': '7px solid blue',
-            'border-style': 'inset',
-            'padding': '50px',
-            'margin': '20px',
-            'width': 300,
-            'font-family': '-apple-system,system-ui,"Segoe UI",Roboto,"Helvetica Neue",Ubuntu,sans-serif',
-            'font-size': '20px',
-            'margin-left': '4%',
-            'margin-top': '2.4%',
-            'display': 'inline-flex'}),
-
-    html.A(
-        href="https://media-exp1.licdn.com/dms/image/C4E03AQFO8pbKx2E8kA/profile-displayphoto-shrink_200_200/0"
-             "/1625566409743?e=1652918400&v=beta&t=H8CKBf6uDoTSmNdq3uoK79X8bdEF_Vt6R8d7jZMfsMU",
-        target='_blank',
-        children='Want to know how the algorithm works?',
-        style={
-            'textAlign': 'left',
-            'color': 'black',
-            'border': '5px solid blue',
-            'border-style': 'dashed solid',
-            'padding': '40px',
-            'margin': '20px',
-            'width': 270,
-            'margin-left': '50%',
-            'margin-top': '1%'
-
-        }
+app.layout = html.Div([
+    dcc.Location(id='url', refresh=False),
+    dbc.NavbarSimple(
+        children=[
+            html.P("You give us data, We find your reward!", className='navText'),
+            html.P("FindYourReward", className='navText1'),
+        ],
+        # brand="FindYourReward",
+        # brand_href="#",
+        color="primary",
+        dark=True,
+        style={'padding': '4.5%'},
     ),
-    html.Button('INPUT YOUR DATA!', id='submit-val', n_clicks=0,
-                style={
-                    'appearance': 'button',
-                    'backface-visibility': 'hidden',
-                    'background-color': '#405cf5',
-                    'border-radius': '6px',
-                    'border-width': '0',
-                    'box-shadow': 'rgba(50, 50, 93, .1) 0 0 0 1px inset,rgba(50, 50, 93, .1) 0 2px 5px 0,rgba(0, 0, '
-                                  '0, .07) 0 1px 1px 0',
-                    'box-sizing': 'border-box',
-                    'color': '#fff',
-                    'cursor': 'pointer',
-                    'font-family': '-apple-system,system-ui,"Segoe UI",Roboto,"Helvetica Neue",Ubuntu,sans-serif',
-                    'font-size': '150%',
-                    'height': '44px',
-                    'line-height': 1.15,
-                    'margin': '12px',
-                    'outline': 'none',
-                    'overflow': 'hidden',
-                    'padding': '30px',
-                    'position': 'relative',
-                    'text-align': 'center',
-                    'text-transform': 'none',
-                    'user-select': 'none',
-                    '-webkit-user-select': 'none',
-                    'touch-action': 'manipulation',
-                    'width': '20%',
-                    'left': '40%',
-                    'margin-top': '-5%'
-                }),
-
-    html.Button('View On Generated Data', id='submit-val1', n_clicks=0,
-                style={
-
-                    'appearance': 'button',
-                    'align-items': 'center',
-                    'background-color': '#0A66C2',
-                    'border': 0,
-                    'border-radius': '100px',
-                    'box-sizing': 'border-box',
-                    'color': '#ffffff',
-                    'cursor': 'pointer',
-                    'display': 'inline-flex',
-                    'font-family': '-apple-system,system-ui,"Segoe UI",Roboto,"Helvetica Neue",Ubuntu,sans-serif',
-                    'font-size': '16px',
-                    'font-weight': 600,
-                    'justify-content': 'center',
-                    'line-height': '20px',
-                    'max-width': '480px',
-                    'min-height': '40px',
-                    'min-width': '0px',
-                    'overflow': 'hidden',
-                    'padding': '10px',
-                    'padding-left': '20px',
-                    'adding-right': '20px',
-                    'text-align': 'center',
-                    'touch-action': 'manipulation',
-                    'transition': 'background-color 0.167s cubic-bezier(0.4, 0, 0.2, 1) 0s, box-shadow 0.167s '
-                                  'cubic-bezier(0.4, 0, 0.2, 1) 0s, color 0.167s cubic-bezier(0.4, 0, 0.2, 1) 0s',
-                    'user-select': 'none',
-                    '-webkit-user-select': 'none',
-                    'vertical-align': 'middle',
-                    'width': '15%',
-                    'margin-left': '78.5%',
-                    'margin-top': '-3.5%'
-                })
+    html.Div(id="page-content", style={
+        'background-image': 'url(https://www.xmple.com/wallpaper/blue-gradient-white-linear-1920x1080-c2-87cefa-ffffff-a-270-f-14.svg)'
+    }, className='divOverlay')
 
 ])
 
+index_page = html.Div([
+    dbc.Card([
+        dbc.CardBody([html.P(
+            "FindYourReward is a tool which enables user to input data, define KPIs and recieve analysis on best reward-giving advertisement.",
+            className="card-text")])
+    ], style={"width": "30rem"}),
+    dbc.Button("Input Data", id="open", outline=True, color="primary", className='inputDataButton'),
+    dbc.Modal(
+        [
+            dbc.ModalHeader(dbc.ModalTitle("Please specify the following information")),
+            dbc.InputGroup(
+                [dbc.InputGroupText("Threshold*:"), dbc.Input(type='number')],
+                className="mb-3",
+
+            ),
+            dbc.InputGroup(
+                [dbc.InputGroupText("Cost Per Arm:"), dbc.Input(type='number'),
+                 dbc.InputGroupText("$")],
+                className="mb-3",
+
+            ),
+            dbc.InputGroup(
+                [dbc.InputGroupText("Exploration time"), dbc.Input(type='number'),
+                 dbc.InputGroupText("Sec."), ],
+                className="mb-3",
+
+            ),
+            dbc.ModalBody("* is required"),
+
+            html.Div([  # this code section taken from Dash docs https://dash.plotly.com/dash-core-components/upload
+                dcc.Upload(
+                    id='upload-data',
+                    children=html.Div([
+                        'Drag and Drop or ',
+                        html.A('Select your CSV Files')
+                    ]),
+                    style={
+                        'width': '100%',
+                        'height': '60px',
+                        'lineHeight': '60px',
+                        'borderWidth': '1px',
+                        'borderStyle': 'dashed',
+                        'borderRadius': '5px',
+                        'textAlign': 'center',
+                        'margin': '0px'
+                    },
+                    # Allow multiple files to be uploaded
+                    multiple=True
+                ),
+                html.Div(id='output-div'),
+                html.Div(id='output-datatable'),
+            ]),
+
+            dbc.ModalFooter(
+                dbc.Button(
+                    "Close", id="close", className="ms-auto", n_clicks=0,
+
+                )
+            ),
+        ],
+        id="modal",
+        is_open=False,
+    ),
+
+    dbc.Card([
+        dbc.CardBody([
+            html.P("For more information: "),
+            dcc.Link(dbc.Button("Click here", outline=True, color="primary"), href="https://www.google.com",
+                     target='blank'),
+        ])
+    ], style={"width": "25rem"}, className='inputMore'),
+    dbc.Card([
+        dbc.CardBody([
+            html.P("View how it works on randomly generated data: "),
+            dcc.Link(dbc.Button("Analyse generated data", outline=True, color="primary"), href="/vispage"),
+        ])
+    ], style={"width": "25rem"}, className='analyseBox'),
+], className="homeCont cont"),
+
+df = px.data.iris()  # iris is a pandas DataFrame
+fig = px.scatter(df, x="sepal_width", y="sepal_length"),
+popover_children = "I am a poopover!"
+
+vis_page = html.Div([
+
+    html.Div([
+
+        html.Div([
+            dbc.Button(
+                "i",
+                id="hover-target",
+                color="white",
+                className="me-1",
+                n_clicks=0,
+                style={'height': '30px', 'weight': '30px', 'textAlign': 'left'}
+            ),
+            dbc.Popover(
+                popover_children,
+                target="hover-target",
+                body=True,
+                trigger="hover"),
+
+            dcc.Graph(
+                figure={
+                    'data': [fig],
+                }, className='dynGraph1',
+            ),
+        ]),
+
+        html.Div([
+            dbc.Button(
+                "i",
+                id="hover-target1",
+                color="white",
+                className="me-1",
+                n_clicks=0
+
+            ),
+            dbc.Popover(
+                popover_children,
+                target="hover-target1",
+                body=True,
+                trigger="hover"),
+
+            dcc.Graph(
+                figure={
+                    'data': [fig],
+                }, className='dynGraph2'
+            ),
+        ]),
+    ], className="graphCont"),
+
+    dbc.Card([
+        dbc.CardBody([
+            html.P(
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
+        ])
+    ], style={"width": "25rem"}, className='analysis'),
+    dbc.Button("Calculate Waste", id='open', color="primary", className='calcwasteButton'),
+    dbc.Modal(
+        [
+            dbc.ModalHeader(dbc.ModalTitle("Header")),
+            dbc.ModalBody("This is the content of the modal"),
+            dbc.ModalFooter(
+                dbc.Button(
+                    "Close", id="close", className="ms-auto", n_clicks=0
+                )
+            ),
+        ],
+        id="modal",
+        is_open=False,
+    ),
+
+    dbc.Button("Download Output", outline=False, color="primary", id="js", n_clicks=0, className='downloadButton'),
+    dcc.Link(dbc.Button("Back to Home", outline=False, color="primary"), href="/homepage", className='backHome')
+
+], className="cont", id="print"),
+
+app.clientside_callback(
+    '''
+    function(n_clicks){
+        if(n_clicks > 0){
+            var opt = {
+                margin: 1,
+                filename: 'myfile.pdf',
+                image: { type: 'jpeg', quality: 0.98 },
+                html2canvas: { scale: 3},
+                jsPDF: { unit: 'cm', format: 'a2', orientation: 'p' },
+                pagebreak: { mode: ['avoid-all'] }
+            };
+            html2pdf().from(document.getElementById("print")).set(opt).toPdf().save();
+        }
+    }
+    ''',
+    Output('js', 'n_clicks'),
+    Input('js', 'n_clicks'),
+)
+
+err_page = html.Div([
+    html.H1('You have entered wrong page')
+]),
+
+
+@app.callback(Output('page-content', 'children'),
+              [Input('url', 'pathname')])
+def displayPage(pathname):
+    if pathname == '/homepage':
+        return index_page
+    elif pathname == '/vispage':
+        return vis_page
+    else:
+        return err_page
+
 
 @app.callback(
-    Input('submit-val', 'n_clicks')
+    Output("modal", "is_open"),
+    [Input("open", "n_clicks"), Input("close", "n_clicks")],
+    [State("modal", "is_open")],
 )
-def update_output(n_clicks):
-    return 'The input value was "{}" and the button has been clicked {} times'.format(
-        n_clicks
-    )
+def toggle_modal(n1, n2, is_open):
+    if n1 or n2:
+        return not is_open
+    return is_open
+
+
+def parse_contents(contents, filename, date):
+    content_type, content_string = contents.split(',')
+
+    decoded = base64.b64decode(content_string)
+    if 'csv' in filename:
+        # Assume that the user uploaded a CSV file 
+        print('There was an error processing this file.')
+        df = pd.read_csv(
+            io.StringIO(decoded.decode('utf-8')))
+    elif 'xls' in filename:
+        # Assume that the user uploaded an excel file 
+        df = pd.read_excel(io.BytesIO(decoded))
+    else:
+        return html.Div([
+            'There was an error processing this file.'
+        ])
+    return dcc.Link(dbc.Button("Analyse Data", outline=False, color="primary"), href="/vispage", className='adata')
+
+
+@app.callback(Output('output-datatable', 'children'),
+              Input('upload-data', 'contents'),
+              State('upload-data', 'filename'),
+              State('upload-data', 'last_modified'))
+def update_output(list_of_contents, list_of_names, list_of_dates):
+    if list_of_contents is not None:
+        children = [
+            parse_contents(c, n, d) for c, n, d in
+            zip(list_of_contents, list_of_names, list_of_dates)]
+        return children
 
 
 if __name__ == '__main__':
